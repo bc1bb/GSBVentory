025dbe8b37e2667442ca57996dc94bc88de11d10
jusdepatate <jusdepatate@protonmail.com>
2023-09-18 22:09:46 +0200

    login working

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GSBVentory
 
 - bg is [free of right](https://pixabay.com/fr/photos/une-analyse-biochimie-biologiste-2030265/)
+
+## env
+- `BACKEND_URL` (ex: `http://localhost:3000`)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,36 @@
-<script>
+<script lang="ts">
 	import Gsb from "$lib/images/gsb.png";
+	import {PUBLIC_BACKEND_URL} from "$env/static/public";
+
+	const handleSubmit = async (event: Event) => {
+		event.preventDefault();
+
+		let username = (document.getElementById("username") as HTMLInputElement).value;
+		let password = (document.getElementById("password") as HTMLInputElement).value;
+
+		const details = {username, password};
+		//@ts-ignore
+		const formBody = Object.keys(details).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+
+		try {
+			const response = await fetch(PUBLIC_BACKEND_URL + "/login", {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+				},
+				body: formBody,
+			});
+
+			if (response.ok) {
+				const token = JSON.parse(JSON.stringify(await response.json())).token;
+				// What in the fucking hell is this absolute fucking piece of trash
+
+				document.cookie = `token=${token}; path=/; max-age=1209600`; // 14 days
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
 </script>
 <svelte:head>
 	<title>GSBVentory</title>
@@ -14,7 +45,7 @@
 				<img src={Gsb} alt="GSB Logo" class="flex mb-5 h-[10vh]">
 				<div class="w-1/3"></div>
 			</div>
-			<form class="w-full max-w-sm">
+			<form class="w-full max-w-sm" on:submit|preventDefault={handleSubmit}>
 				<div class="md:flex md:items-center mb-6">
 					<div class="md:w-1/3">
 						<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="username">
@@ -22,7 +53,7 @@
 						</label>
 					</div>
 					<div class="md:w-2/3">
-						<input class="hover:shadow-lg bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="username" type="text">
+						<input required class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="username" type="text">
 					</div>
 				</div>
 				<div class="md:flex md:items-center mb-6">
@@ -32,12 +63,12 @@
 						</label>
 					</div>
 					<div class="md:w-2/3">
-						<input class="hover:shadow-lg bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" type="password" placeholder="******************">
+						<input required class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" type="password" placeholder="************">
 					</div>
 				</div>
 				<div class="md:flex md:items-center">
 					<div class="md:w-2/3 flex grow">
-						<button class="hover:shadow-lg border-2 border-theme-darkgrey font-bold py-2 px-4 rounded text-center grow " type="button">
+						<button class="border-2 border-theme-darkgrey font-bold py-2 px-4 rounded text-center grow" type="submit">
 							Login
 						</button>
 					</div>
