55d656b2ce264f27a43de2039ab3fbd73b468774
jusdepatate <jusdepatate@protonmail.com>
2023-09-29 22:06:38 +0200

    introduce new function dateHtmlValue()

---
+++ src/scripts/dateHtmlValue.ts
@@ -0,0 +1,10 @@
+const dateHtmlValue = (date: Date) => {
+    // for some reason React likes to joke around and send string to this function while everything is typed as Date,
+    // i love typescript btw.
+    const newDate = new Date(date);
+
+    if (newDate.getFullYear() === 1970) return ""
+    else newDate.toLocaleDateString('en-CA');
+}
+
+export default dateHtmlValue;
