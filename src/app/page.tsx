395238613ce04897569ec3867bebf1c9afb25f85
jusdepatate <jusdepatate@protonmail.com>
2023-09-29 12:47:31 +0200

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,39 +45,39 @@ export default function Home() {
             <div className="flex">
               <div className="w-1/3"></div>
               <img src="/gsb.png" alt="GSB Logo" className="flex mb-5 h-[10vh]" />
-                <div className="w-1/3"></div>
+              <div className="w-1/3"></div>
             </div>
             <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-            <div className="md:flex md:items-center mb-6">
-              <div className="md:w-1/3">
-                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="username">
-                  User
-                </label>
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="username">
+                    User
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input required className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="username" type="text" />
+                </div>
               </div>
-              <div className="md:w-2/3">
-                <input required className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="username" type="text" />
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="password">
+                    Password
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input required className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" type="password" placeholder="************" />
+                </div>
               </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-              <div className="md:w-1/3">
-                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="password">
-                  Password
-                </label>
+              <div className="md:flex md:items-center">
+                <div className="md:w-2/3 flex grow">
+                  <button className="border-2 border-theme-darkgrey font-bold py-2 px-4 rounded text-center grow" type="submit">
+                    Login
+                  </button>
+                </div>
               </div>
-              <div className="md:w-2/3">
-                <input required className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="password" type="password" placeholder="************" />
-              </div>
-            </div>
-            <div className="md:flex md:items-center">
-              <div className="md:w-2/3 flex grow">
-                <button className="border-2 border-theme-darkgrey font-bold py-2 px-4 rounded text-center grow" type="submit">
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
-</section>
+      </section>
   )
 }
