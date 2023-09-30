b8d62325c794464ac01df58c5c1a5c3053c057c6
jusdepatate <jusdepatate@protonmail.com>
2023-09-30 14:47:14 +0200

    way better handling of stuff on /hmu/*/

--- src/app/hmu/[toBeUpdated]/page.tsx
+++ src/app/hmu/[toBeUpdated]/page.tsx
@@ -14,12 +14,13 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
     const newHardware = (params.toBeUpdated == "new");
 
     const [internalId, setInternalId] = useState("");
+    const [internalIdForm, setInternalIdForm] = useState(<></>);
     const [type, setType] = useState("");
-    const [buyDate, setBuyDate] = useState(new Date());
+    const [buyDate, setBuyDate] = useState("");
     const [serialNumber, setSerialNumber] = useState("");
     const [manufacturer, setManufacturer] = useState("");
     const [model, setModel] = useState("");
-    const [endOfWarrantyDate, setEndOfWarrantyDate] = useState(new Date());
+    const [endOfWarrantyDate, setEndOfWarrantyDate] = useState("");
     const [note, setNote] = useState("");
 
     useEffect(() => {
@@ -46,8 +47,7 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
         // From now on only new hardware
         fetchHardware(document.cookie, params.toBeUpdated).then(
             (hardware) => {
-                setTypesAsOptions(<input disabled value={hardware.type} id="type" name="type" className="capitalize h-10 border mt-1 rounded px-4 w-full bg-gray-50"></input>);
-
+                setTypesAsOptions(<input value={hardware.type} id="type" name="type" readOnly={true} className="capitalize text-gray-400 h-10 border mt-1 rounded px-4 w-full bg-gray-50" />);
 
                 setSerialNumber(hardware.serialNumber);
                 setInternalId(hardware.internalId);
@@ -55,8 +55,10 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
                 setModel(hardware.model);
                 setNote(hardware.note);
 
-                if (new Date(hardware.buyDate).getFullYear() !== 1970) setBuyDate(new Date(hardware.buyDate));
-                if (new Date(hardware.endOfWarrantyDate).getFullYear() !== 1970) setEndOfWarrantyDate(new Date(hardware.endOfWarrantyDate));
+                setInternalIdForm(<input type="text" value={internalId} readOnly={true} name="internalId" id="internalId" className="invisible h-0 w-0" />)
+
+                if (new Date(hardware.buyDate).getFullYear() !== 1970) setBuyDate(dateHtmlValue(hardware.buyDate));
+                if (new Date(hardware.endOfWarrantyDate).getFullYear() !== 1970) setEndOfWarrantyDate(dateHtmlValue(hardware.endOfWarrantyDate));
             }
         ).catch(console.error);
     }, []);
@@ -64,21 +66,22 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        setType((document.getElementById("type") as HTMLInputElement).value);
-        setBuyDate(new Date((document.getElementById("buy_date") as HTMLInputElement).value));
-        setSerialNumber((document.getElementById("serial_number") as HTMLInputElement).value);
-        setManufacturer((document.getElementById("manufacturer") as HTMLInputElement).value);
-        setModel((document.getElementById("model") as HTMLInputElement).value);
-        setEndOfWarrantyDate(new Date((document.getElementById("end_of_warranty_date") as HTMLInputElement).value));
-        setNote((document.getElementById("note") as HTMLInputElement).value);
+        // new hardware is a POST, edit on hardware is PATCH
+        const method = newHardware ? "POST" : "PATCH";
 
-        const details = {type, buyDate, serialNumber, manufacturer, model, endOfWarrantyDate, note};
+        let details: object
+        if (newHardware)
+            details = {type, buyDate, serialNumber, manufacturer, model, endOfWarrantyDate, note};
+        else
+            details = {type, internalId, buyDate, serialNumber, manufacturer, model, endOfWarrantyDate, note};
+
         //@ts-ignore
         const formBody = Object.keys(details).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+        console.log(formBody);
 
         try {
             const response = await fetch(PUBLIC_BACKEND_URL + "/hmu", {
-                method: 'POST',
+                method: method,
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
                     'Authorization': getCookie("token", document.cookie)
@@ -108,6 +111,7 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
                                 </div>
 
                                 <form className="lg:col-span-2" onSubmit={handleSubmit}>
+                                    {internalIdForm}
                                     <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                                         <div className="md:col-span-5">
                                             <label htmlFor="type">Type</label>
@@ -116,32 +120,32 @@ const ToBeUpdated = ({params}: {params: { toBeUpdated: string }}) => {
 
                                         <div className="md:col-span-3">
                                             <label htmlFor="end_of_warranty_date">End of warranty</label>
-                                            <input type="date" defaultValue={dateHtmlValue(endOfWarrantyDate)} name="end_of_warranty_date" id="end_of_warranty_date" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                                            <input type="date" value={endOfWarrantyDate} onChange={e=>setEndOfWarrantyDate(dateHtmlValue(e.target.value))} name="end_of_warranty_date" id="end_of_warranty_date" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                                         </div>
 
                                         <div className="md:col-span-2">
                                             <label htmlFor="buy_date">Date of buy</label>
-                                            <input type="date" defaultValue={dateHtmlValue(buyDate)} name="buy_date" id="buy_date" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
+                                            <input type="date" value={buyDate} onChange={e=>setBuyDate(dateHtmlValue(e.target.value))} name="buy_date" id="buy_date" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
                                         </div>
 
                                         <div className="md:col-span-2">
                                             <label htmlFor="manufacturer">Manufacturer</label>
-                                            <input type="text" defaultValue={manufacturer} name="manufacturer" id="manufacturer" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
+                                            <input type="text" value={manufacturer} onChange={e=>setManufacturer(e.target.value)} name="manufacturer" id="manufacturer" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
                                         </div>
 
                                         <div className="md:col-span-3">
                                             <label htmlFor="model">Model</label>
-                                            <input type="text" defaultValue={model} name="model" id="model" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
+                                            <input type="text" value={model} onChange={e=>setModel(e.target.value)} name="model" id="model" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
                                         </div>
 
                                         <div className="md:col-span-5">
                                             <label htmlFor="serial_number">Serial Number</label>
-                                            <input type="text" defaultValue={serialNumber} name="serial_number" id="serial_number" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
+                                            <input type="text" value={serialNumber} onChange={e=>setSerialNumber(e.target.value)} name="serial_number" id="serial_number" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" required />
                                         </div>
 
                                         <div className="md:col-span-5">
                                             <label htmlFor="note">Note</label>
-                                            <textarea name="note" defaultValue={note} id="note" className="h-20 border mt-1 rounded px-4 w-full bg-gray-50 resize-none" />
+                                            <textarea name="note" value={note} onChange={e=>setNote(e.target.value)} id="note" className="h-20 border mt-1 rounded px-4 w-full bg-gray-50 resize-none" />
                                         </div>
 
                                         <div className="md:col-span-5">
